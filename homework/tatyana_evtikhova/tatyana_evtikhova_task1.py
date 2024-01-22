e79fc6384262e59018ff2aa21fab17e5f4f127c0
Tatyana Evtikhova <tatyanaevtihova@gmail.com>
2024-01-22 19:02:40 +0000

    Tatyana Evtikhova Homework 3 done

---
+++ homework/tatyana_evtikhova/tatyana_evtikhova_task1.py
@@ -0,0 +1,7 @@
+#Даны 2 числа a и b. Получить их сумму, разность и произведение
+num1 = 35
+num2 = 9
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+
---
+++ homework/tatyana_evtikhova/tatyana_evtikhova_task2.py
@@ -0,0 +1,5 @@
+#Даны числа x и y. Получить x − y / 1 + xy
+
+num1 = 222
+num2 = 13
+print(num1 -num2 / 1 + num1 * num2)
